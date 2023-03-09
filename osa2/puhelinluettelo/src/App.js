be55fdba443a48b35e5241171568ab81e3cc23b4
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-09 14:32:15 +0200

    tehtävä 2.9 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,12 +8,17 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   // uusi numero
   const [newNumber, setNewNumber] = useState('')
+  //filtteri
+  const [filter, setFilter] = useState('')
+  const peopleToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   //lisää tapahtumankäsittelijä nimen lisäämiselle
   const addPerson = (event) => {
@@ -52,18 +57,29 @@ const App = () => {
 
   // tapahtumankäsittelijät syötekomponenttien muutoksille
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+    
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+        value={filter}
+        onChange={handleFilterChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -81,7 +97,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {/*tulosta henkilön tiedot*/}
-      {persons.map(person =>
+      {peopleToShow.map(person =>
         <Person key={person.name} person={person} />)}
     </div>
   )
