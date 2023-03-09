29ab720e97c2bda438729d838284002523266e15
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-09 12:55:27 +0200

    tehtävä 2.6 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,54 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  //lisää tapahtumankäsittelijä nimen lisäämiselle
+  const addPerson = (event) => {
+    // laita lisäämään nimi taulukkoon
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    console.log('button clicked', event.target)
+  }
+
+  // lisää tapahtumankäsittelijä syötekomponentin muutokselle
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName} 
+          onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/*tulosta henkilön tiedot*/}
+      {persons.map(person =>
+        <Person key={person.name} person={person} />)}
     </div>
   )
 
