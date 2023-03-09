5b7633b02f694e9113b383fe28520e07e9e72da7
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-09 14:07:49 +0200

    tehtävä 2.8 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,43 +2,63 @@ import { useState } from 'react'
 
 const Person = ({ person }) => {
   return (
-    <p>{person.name}</p>
+    <p>{person.name} {person.number}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1231244' }
   ]) 
   const [newName, setNewName] = useState('')
+  // uusi numero
+  const [newNumber, setNewNumber] = useState('')
 
   //lisää tapahtumankäsittelijä nimen lisäämiselle
   const addPerson = (event) => {
     event.preventDefault()
+
+    if (newName === ''){
+      alert(`name field empty`)
+      return
+    }
+    if (newNumber === ''){
+      alert(`number field empty`)
+      return
+    }
 
     // hae syöte taulukosta, jos ei löydy niin lisää annettu syöte
     if (persons.find(person => person.name === newName)) {
       console.log('uh-oh!')
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
       return
     }
 
     // lisää nimi taulukkoon
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     console.log('lisätty!')
 
     setNewName('')
+    setNewNumber('')
     console.log('button clicked', event.target)
   }
 
-  // lisää tapahtumankäsittelijä syötekomponentin muutokselle
+  // tapahtumankäsittelijät syötekomponenttien muutoksille
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -52,8 +72,12 @@ const App = () => {
           />
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
         </div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       {/*tulosta henkilön tiedot*/}
