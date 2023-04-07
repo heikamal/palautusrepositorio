0349c9f118496d90f5df1018c6a8440f9db4cd4b
Heikki Malkavaara <heimal@student.uef.fi>
2023-04-07 15:44:47 +0300

    tehtävä 6.21 tehty

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery } from 'react-query'
-import axios from 'axios'
+import { getAnecdotes } from './requests'
 
 const App = () => {
 
@@ -9,12 +9,8 @@ const App = () => {
     console.log('vote')
   }
 
-  const getAnecdotes = () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  const result = useQuery('anecdotes', getAnecdotes, { retry: 1, refetchOnWindowFocus: false })
 
-  const result = useQuery(
-    'anecdotes', getAnecdotes, { retry: 1}
-    
-  )
   console.log(result)
 
   if ( result.isLoading ) {
