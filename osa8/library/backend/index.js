26b097a7fa76ab2ba7ba505a3ab607fcce369259
Heikki Malkavaara <heimal@student.uef.fi>
2023-04-12 15:56:33 +0300

    mongodb-yhteys luotu

--- osa8/library/backend/index.js
+++ osa8/library/backend/index.js
@@ -2,122 +2,45 @@ const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+require('dotenv').config()
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const typeDefs = `
   type Book {
     title: String!
-    published: Int
-    author: String
+    published: Int!
+    author: Author!
+    genres: [String!]!
     id: ID!
-    genres: [String]
   }
 
   type Author {
     name: String!
     id: ID!
     born: Int
-    bookCount: Int
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String, genre: String): [Book!]!
+    allBooks: [Book!]!
     allAuthors: [Author!]!
   }
   type Mutation {
@@ -137,49 +60,39 @@ const typeDefs = `
 const resolvers = {
   Query: {
     // palauta taulukoiden pituudet
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
     // palauta kaikki kirjat
-    allBooks: (root, args) => {
-      // jos kirjailija ollaan annettu
-      const authorBooks = args.author 
-        ? books.filter(book => book.author === args.author) 
-        : books
-      // jos genre ollaan annettu
-      const genreBooks = args.genre 
-        ? authorBooks.filter(book => book.genres.includes(args.genre)) 
-        : authorBooks
-      return genreBooks
+    
+    allBooks:  async (root, args) => {
+      // TODO: filters
+      return Book.find({})
     },
     //palauta kaikki kirjailijat
-    allAuthors: () => authors,
+    allAuthors: async (root, args) => {
+      return Author.find({})
+    },
   },
-  // kirjoitetaan funktio hakemaan jokaiselle kirjailijalle kaikki tämän kirjoittamat kirjat
+  /*// kirjoitetaan funktio hakemaan jokaiselle kirjailijalle kaikki tämän kirjoittamat kirjat
   Author: {
     bookCount: async (root) => {
+      // TODO: make this work!
       // luo uusi taulukko suodattamalla kaikki 
       const authorBooks = books.filter(book => book.author === root.name)
       return authorBooks.length
     } 
-  },
+  },*/
   Mutation: {
     addBook: (root, args) => {
       // tee kirjasta oma olionsa
-      const book = {...args, id: uuid() }
+      const book = new Book({ ...args })
       // tarkista onko kirjailija jo kannassa
-      const names = authors.map(author => author.name)
-      if (!names.includes(book.author)) {
         // tee kirjailijasta oma olionsa
-        const author = {
-          name: book.author,
-          id: uuid()
-        }
         // lisää se kantaan
-        authors = authors.concat(author)
-      }
-      // lisää se kantaan
-      books = books.concat(book)
-      return book
+      
+      // hae kirjailija kannasta ja lisää se kirjaan
+      // lisää kirja kantaan
+      return book.save()
     },
     editAuthor: (root, args) => {
       // etsi kirjailija nimen perusteella
