d0e0e7a1e677fe780578e4cef1813eaa58c0cf6a
Heikki Malkavaara <heimal@student.uef.fi>
2023-04-25 16:13:00 +0300

    displays data

--- osa9/flight-diary-frontend/src/App.tsx
+++ osa9/flight-diary-frontend/src/App.tsx
@@ -1,8 +1,43 @@
-
+import { useState } from "react";
+import { Diary } from "./types";
 const App = () => {
+  const [diaries, setDiaries] = useState<Diary[]>([
+    {
+      "id": 1,
+      "date": "2017-01-01",
+      "weather": "rainy",
+      "visibility": "poor"
+    },
+    {
+      "id": 2,
+      "date": "2017-04-01",
+      "weather": "sunny",
+      "visibility": "good"
+    },
+    {
+      "id": 3,
+      "date": "2017-04-15",
+      "weather": "windy",
+      "visibility": "good"
+    },
+    {
+      "id": 4,
+      "date": "2017-05-11",
+      "weather": "cloudy",
+      "visibility": "good"
+    }
+  ]);
+
   return (
     <div>
-      Hello World
+      <h2>Diary entries</h2>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {diaries.map(diary => <li key={diary.id}>
+          <h3>{diary.date}</h3>
+          visibility: {diary.visibility}<br/>
+          weather: {diary.weather}
+          </li>)}
+      </ul>
     </div>
   );
 }
