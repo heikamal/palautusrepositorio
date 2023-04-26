7eef50a9f03406b6b0d7c42074805e69a8952845
Heikki Malkavaara <heimal@student.uef.fi>
2023-04-26 11:56:22 +0300

    tehtävä 9.18 tehty

--- osa9/flight-diary-frontend/src/components/DiaryForm.tsx
+++ osa9/flight-diary-frontend/src/components/DiaryForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import { Diary } from "../types";
 import { createDiary } from "../services/diaryService";
+import axios from "axios";
 
 interface DiaryFormProps {
 	diaries: Diary[],
@@ -12,13 +13,22 @@ const DiaryForm = (props: DiaryFormProps) => {
 	const [visibility, setVisibility] = useState('');
 	const [weather, setWeather] = useState('');
 	const [comment, setComment] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
 
-	const addFormHandler = (event: FormEvent<HTMLFormElement>) => {
+	const addFormHandler = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log('merkintä lisätty!');
-		createDiary({ date: date, visibility: visibility, weather: weather, comment: comment }).then(data => {
-			props.setDiaries(props.diaries.concat(data)) 
-		});
+		try {
+			const data = await createDiary({ date: date, visibility: visibility, weather: weather, comment: comment });
+			props.setDiaries(props.diaries.concat(data)) ;
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				console.log(error.status);
+				console.log(error.response);
+				if (error.response){showError(error.response.data);}
+			} else {
+				console.error(error);
+			}
+		}
 
 		setDate('');
 		setVisibility('');
@@ -26,9 +36,15 @@ const DiaryForm = (props: DiaryFormProps) => {
 		setComment('');
 	};
 
+	const showError = (message: string) => {
+		setErrorMessage(message);
+		setTimeout(() => setErrorMessage(''), 5000)
+	}
+
 	return (
 		<div>
 			<h3>Add new entry</h3>
+			{errorMessage !== '' && <p style={{ color: "#FF0000" }}>{errorMessage}</p>}
 			<form onSubmit={addFormHandler}>
 				date <input type="text" value={date} onChange={(event) => setDate(event.currentTarget.value)}></input><br/>
 				visibility <input type="text" value={visibility} onChange={(event) => setVisibility(event.currentTarget.value)}></input><br/>
